3db3d09409317b4f88439b5b7866211ebe413b2e
vanshika1807 <67184259+vanshika1807@users.noreply.github.com>
2026-05-02 14:56:56 +0530

    Add user information table structure
    Added a table structure for user information.

--- SQLZoo/1_SELECT_name.sql
+++ SQLZoo/1_SELECT_name.sql
@@ -1,4 +1,7 @@
-
 --Pattern Matching Strings
 --This tutorial uses the LIKE operator to check names. We will be using the SELECT command on the table worl
 --Find the country that start with Y
+| user_id | username | status |
+| :--- | :--- | :--- |
+| 1 | jdoe | active |
+| 2 | asmith | inactive |
